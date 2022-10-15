0d77cd6533478f894f2b9034dc462a3c85819289
GPraveenVarma <105558430+GPraveenVarma@users.noreply.github.com>
2022-10-15 19:53:51 -0400

    test2

--- src/main/java/mypack/helloworld.java
+++ src/main/java/mypack/helloworld.java
@@ -3,6 +3,6 @@ package mypack;
 public class helloworld {
     public static void main(String args[])
     {
-        System.out.println("git push");
+        System.out.println("git push success");
     }
 }
