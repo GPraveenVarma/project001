0777ad59f76663793bea13778077b71459c5390c
GPraveenVarma <105558430+GPraveenVarma@users.noreply.github.com>
2022-10-15 21:52:54 -0400

    modified

--- src/main/java/mypack/helloworld.java
+++ src/main/java/mypack/helloworld.java
@@ -7,3 +7,11 @@ public class helloworld {
         System.out.println("git push success test");
     }
 }
+
+public class helloworld {
+    public static void main(String args[])
+    {
+        System.out.println("branch1 test");
+        System.out.println("git push success test");
+    }
+}
